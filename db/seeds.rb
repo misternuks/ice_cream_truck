cc9340faa4001a9d5970775d8feccb5ea64ce1e4
Matthew Miner <misternuks@gmail.com>
2024-07-10 20:50:00 +0900

    seed database with customers and food items

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Deleting previous customers..."
+Customer.destroy_all
+puts "...Customers deleted."
+puts "Deleting previous food items..."
+FoodItem.destroy_all
+puts "...Food items deleted."
+
+puts "Creating new customers..."
+
+Customer.create([
+  { name: "Matthew" },
+  { name: "Emma" },
+  { name: "Thomas" }
+])
+puts "...Customers created."
+
+puts "Creating new food items..."
+
+FoodItem.create([
+  { name: "Chocolate Ice Cream", price: 300, stock: 10, category: "Ice Cream" },
+  { name: "Pistachio Ice Cream", price: 300, stock: 5, category: "Ice Cream" },
+  { name: "Strawberry Ice Cream", price: 300, stock: 8, category: "Ice Cream" },
+  { name: "Mint Ice Cream", price: 300, stock: 7, category: "Ice Cream" },
+  { name: "Shaved Ice", price: 150, stock: 20, category: "Shaved Ice" },
+  { name: "Snack Bar", price: 200, stock: 15, category: "Snack Bar" }
+])
+
+puts "...Food items created."
+puts "All seeds planted."
